8a728e8221fc417ced93d39b56f1269e33c5d440
Junerey Casuga <junereycasuga@Tagbonds-MacBook-Pro-2.local>
2015-01-09 09:26:09 +0800

    change data source for usd value of btc

--- vskype-btc-usd.php
+++ vskype-btc-usd.php
@@ -5,14 +5,16 @@ require_once 'firebase/firebaseLib.php';
 // create a firebase instance
 $firebase = new Firebase('https://vskype.firebaseio.com/btc_usd','bcwy4lyI7bwI8J1IpWhJxR1hsFQjNEkfEtPYWK34');
 
-$url = 'https://www.bitstamp.net/api/ticker/';
+// $url = 'https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/';
+$url = 'https://blockchain.info/ticker';
 $curl = curl_init();
 
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
 $response = json_decode(curl_exec($curl));
-$firebase->set('price', $response->last);
+// print_r($response->USD->last);exit;
+$firebase->set('price', $response->USD->last);
 
 curl_close($curl);
 ?>
